f587770a902cfa6e2c60c2ee3d1bc008c152b7cb
Николай Федоров <nvfedorov@cloudmts.ru>
2021-07-27 10:05:28 +0300

    ls1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
+    <img alt="Vue logo" :src="src">
     <img alt="Vue logo" src="./assets/logo.png">
+    {{ src }}
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calculator />
   </div>
 </template>
 
 <script>
+import Logo from './assets/logo.png'
 import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './components/Calculator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Calculator
+  },
+  data() {
+    return {
+      src: Logo
+    }
+  },
 }
 </script>
 
---
+++ src/components/Calculator.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="calc">
+    {{ message }}
+    {{ message.length }}
+    {{ message.split().reverse().join() }}
+
+    <div>
+      <input
+        type="number"
+        placeholder="Operand1"
+        v-on:focus="onFocus"
+        v-model.number="operand1"
+      />
+      <input
+        type="number"
+        placeholder="Operand2"
+        @keyup="onKeyUp"
+        v-model.number="operand2"
+      />
+      = {{ result }}
+      <div class="keyboard">
+      <button @click="result = operand1 + operand2">+</button>
+      <button @click="div">-</button>
+      <button v-on:click="multi(operand1, operand2)">*</button>
+      <button @click="result = operand1 / operand2">/</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calculator",
+  data: () => ({
+    message: "Hello Vue",
+    operand1: 0,
+    operand2: 0,
+    result: 0,
+    array: []
+  }),
+  methods: {
+    doThan(text, event) {
+      console.log(text, event);
+    },
+    div() {
+        this.result = this.operand1 - this.operand2
+    },
+    multi(op1, op2) {
+        this.result = op1 * op2
+    },
+    onFocus() {
+      console.log("onFocus");
+    },
+    onKeyUp() {
+      console.log("onKeyUp");
+    }
+  }
+};
+</script>
